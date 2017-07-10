d641af428d125e2d5245a6aaeb4c5cd5a426987d
huso51 <huseyinaydin99@gmail.com>
2017-07-10 23:45:15 +0300

    Add files via upload

---
+++ 043_ConvertDateTime/src/java/com/siteadi/YonetimliBean.java
@@ -0,0 +1,33 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.siteadi;
+
+import java.util.Date;
+import javax.faces.bean.ManagedBean;
+import javax.faces.bean.RequestScoped;
+
+/**
+ *
+ * @author husey
+ */
+@ManagedBean
+@RequestScoped
+public class YonetimliBean {
+
+    Date dogumTarihi;
+    public YonetimliBean() {
+    }
+
+    public Date getDogumTarihi() {
+        return dogumTarihi;
+    }
+
+    public void setDogumTarihi(Date dogumTarihi) {
+        this.dogumTarihi = dogumTarihi;
+    }
+    
+    
+}
